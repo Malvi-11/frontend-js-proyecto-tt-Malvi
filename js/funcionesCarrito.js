9d2a1fe2ecab0ccbdd450296b35eba45d67a061e
Malvina Peque <malvinapeque@gmail.com>
2025-11-15 14:37:12 -0300

    importe funciones y cargue img

--- js/funcionesCarrito.js
+++ js/funcionesCarrito.js
@@ -4,3 +4,26 @@ import {
   vaciarCarritoStorage,
 } from "./storage.js";
 import { actualizarContador, mostrarMensaje } from "./ui.js";
+
+export const agregarAlCarrito = (producto) => {
+  const carrito = obtenerCarrito();
+  carrito.push(producto);
+
+  guardarCarrito(carrito);
+  actualizarContador(carrito);
+  mostrarMensaje("Producto agregado al carrito");
+};
+
+export const eliminarProducto = (indice) => {
+  const carrito = obtenerCarrito();
+  carrito.splice(indice, 1);
+
+  guardarCarrito(carrito);
+  actualizarContador(carrito);
+  mostrarMensaje("Producto eliminado del carrito");
+};
+export const vaciarCarrito = () => {
+  vaciarCarritoStorage();
+  actualizarContador([]);
+  mostrarMensaje("Carrito vaciado");
+};
--- js/index.js
+++ js/index.js
@@ -1,2 +1,39 @@
+import { producto, productos } from "./productos.js";
+import { agregarAlCarrito } from " ./funcionesCarrito.js ";
+import { obtenerCarrito } from "./storage.js";
+import { actualizarContador } from "./ui.js";
 
- 
+document.addEventListener("DOMContentLoaded", () => {
+  const contenedor = document.getElementById("contenedor-tarjetas");
+
+  const carrito = obtenerCarrito();
+  actualizarContador(carrito);
+
+  productos.forEach((producto) => {
+    const tarjeta = document.createElement("article");
+    tarjeta.classList.add("tarjeta-producto");
+
+    const img = document.createElement("img");
+    img.src = producto.img;
+    img.alt = producto.nombre;
+
+    const titulo = document.createElement("h3");
+    titulo.textContent = producto.nombre;
+
+    const precio = document.createElement("p");
+    precio.textContent = `$${producto.precio}`;
+
+    const boton = document.createElement("button");
+    boton.classList.add("btn");
+    boton.textContent = "Agregar al carrito";
+
+    boton.addEventListener("click", () => {
+      agregarAlCarrito(producto);
+    });
+    tarjeta.appendChild(img);
+    tarjeta.appendChild(titulo);
+    tarjeta.appendChild(precio);
+    tarjeta.appendChild(boton);
+    contenedor.appendChild(tarjeta);
+  });
+});
