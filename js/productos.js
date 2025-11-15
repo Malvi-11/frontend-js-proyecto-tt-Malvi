beb0c225370eeec0c2e4e78e1511379e489403aa
Malvina Peque <malvinapeque@gmail.com>
2025-11-15 13:09:26 -0300

    creo productos js

--- js/productos.js
+++ js/productos.js
@@ -1,2 +1,6 @@
 export const productos = [
-    {}
+  { id: "1", nombre: "Mate 1", img: "/img/mate1.jpg", precio: 1000 },
+  { id: "2", nombre: "Mate 2", img: "/img/mate2.jpg", precio: 1500 },
+  { id: "3", nombre: "Mate 3", img: "/img/mate3.jpg", precio: 2000 },
+  { id: "4", nombre: "Mate 4", img: "/img/mate4.jpg", precio: 2500 },
+];
