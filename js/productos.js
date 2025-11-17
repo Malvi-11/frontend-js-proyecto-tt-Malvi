b80b1dd08f103284ec3a5f60c68967b5be3f2098
Malvina Peque <malvinapeque@gmail.com>
2025-11-17 12:02:29 -0300

    agregue img

--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,7 @@
 export const productos = [
   { id: "1", nombre: "Mate clásico", img: "img/mate1.jpg", precio: 15000 },
   { id: "2", nombre: "Mate tallado", img: "img/mate2.jpg", precio: 15000 },
-  { id: "3", nombre: "Mate argentina", img: "img/mate3.jpg", precio: 25000 },
+  { id: "3", nombre: "Mate Argentina", img: "img/mate3.jpg", precio: 25000 },
   { id: "4", nombre: "Set de mate", img: "img/mate4.jpg", precio: 30000 },
+  { id: "5", nombre: "Mate y yerbera", img: "img/mate5.jpg", precio: 25000 },
 ];
